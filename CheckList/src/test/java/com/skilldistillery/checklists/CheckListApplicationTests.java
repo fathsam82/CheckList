e8800cd590c29062436420240ea092faaddd9c14
fathsam82 <fathsam82@gmail.com>
2023-09-13 15:11:43 -0600

    Create Spring Boot REST, link JPA project dependency

---
+++ CheckList/src/test/java/com/skilldistillery/checklists/CheckListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.checklists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
